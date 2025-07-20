0ee34074419a43a704eb92fa16541e2cffcade24
sangyun lee <115783758+Sangyun1117@users.noreply.github.com>
2025-07-20 19:01:31 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct state{
+    string st;
+    int count;
+};
+
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+    
+    queue<state> q;
+    vector<bool> visited(words.size(), false);
+    for(int i=0; i<words.size(); ++i){
+        if(begin == words[i])
+            visited[i] = true;
+    }
+    q.push({begin, 0});
+    
+    while(!q.empty()){
+        state now = q.front(); q.pop();
+        string nowSt = now.st;
+        int count = now.count;
+        
+        if(target == nowSt){
+            answer = count;
+            break;
+        }
+        for(int idx = 0; idx < words.size(); idx++){
+            string next = words[idx];
+            if(visited[idx])
+                continue;
+            int different = 0;
+            for(int i=0; i<next.length(); ++i){
+                if(nowSt[i] != next[i])
+                    different++;
+            }
+            if(different==1){
+                q.push({next, count + 1});
+                visited[idx] = true;
+            }
+        }
+    }
+    
+    
+    return answer;
+}
