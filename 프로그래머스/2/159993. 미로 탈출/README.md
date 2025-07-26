821052eebc184b218dc4da04f9291464cbee761e
sangyun lee <115783758+Sangyun1117@users.noreply.github.com>
2025-07-26 16:07:59 +0900

    [level 2] Title: 미로 탈출, Time: 0.40 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,6 +1,6 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=cpp) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 26일 16:07:11
+2025년 07월 26일 16:07:58
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.cpp
@@ -5,68 +5,91 @@ using namespace std;
 
 int dx[4] = {0,0,1,-1};
 int dy[4] = {1,-1,0,0};
-int xLen;
-int yLen;
-struct Pos{
-    int y;
-    int x;
-    int count;
-};
-
-int bfs(Pos start, Pos end, vector<string>& maps){
-    queue<Pos> q;
-    q.push(start);
-    vector<vector<bool>> visited(yLen, vector<bool>(xLen, false));
-    
-    while(!q.empty()){
-        Pos now = q.front(); q.pop();
-        if(now.x==end.x && now.y==end.y){
-            return now.count;
-        }
-        
-        for(int i=0; i<4; ++i){
-            int nextY = now.y + dy[i];
-            int nextX = now.x + dx[i];
-            
-            if(nextY<0||nextY>=yLen||nextX<0||nextX>=xLen)
-                continue;
-            if(visited[nextY][nextX] || maps[nextY][nextX] == 'X')
-                continue;
-            
-            visited[nextY][nextX] = true;
-            q.push({nextY, nextX, now.count + 1});
-        }
-    }
-    return -1;
-}
+using Pos = pair<int, int>;
 
 int solution(vector<string> maps) {
     Pos start;
     Pos lever;
     Pos end;
+    int xLen = maps.size();
+    int yLen = maps[0].size();
     
-    yLen = maps.size();
-    xLen = maps[0].size();
-    
-    for(int i =0; i<yLen; ++i){
-        for(int j=0; j<xLen; ++j){
+    for(int i =0; i<maps.size(); ++i){
+        for(int j=0; j<maps[i].size(); ++j){
             if(maps[i][j] == 'S'){
-                start = {i, j, 0};
+                start.first = i;
+                start.second = j;
             }
             else if(maps[i][j] == 'E'){
-                end = {i, j, 0};
+                end.first = i;
+                end.second = j;
             }
             else if(maps[i][j] == 'L'){
-                lever = {i, j, 0};
+                lever.first = i;
+                lever.second = j;
             }
         }
     }
-    int r1 = bfs(start, lever, maps);
-    if(r1==-1)
+    
+    queue<Pos> q;
+    q.push(start);
+    vector<vector<bool>> visited(xLen,vector<bool>(yLen, false));
+    int count = 0;
+    bool checkLever = false;
+    visited[start.first][start.second] = true;
+    while(!q.empty()){
+        Pos now = q.front(); q.pop();
+        
+        if(now.first==lever.first && now.second==lever.second){
+            checkLever = true;
+            break;
+        }
+        for(int i=0; i<4; ++i){
+            int nextY = now.first + dy[i];
+            int nextX = now.second + dx[i];
+            if(nextY<0||nextY>=maps.size()||nextX<0||nextX>=maps[0].size())
+                continue;
+            if(visited[nextY][nextX]||maps[nextY][nextX]=='X')
+                continue;
+            q.push({nextY, nextX});
+            visited[nextY][nextX] = true;
+            count++;
+        }
+    }
+    if(!checkLever)
         return -1;
-    int r2 = bfs(lever, end, maps);
-    if(r2==-1)
+    
+    bool checkEnd = false;
+    queue<Pos> q2;
+    q2.push(lever);
+    vector<vector<bool>> visited2(xLen,vector<bool>(yLen, false));
+    visited2[lever.first][lever.second] = true;
+    
+    while(!q2.empty()){
+        Pos now = q2.front(); q2.pop();
+        
+        if(now.first==end.first && now.second==end.second){
+            checkEnd = true;
+            break;
+        }
+        for(int i=0; i<4; ++i){
+            int nextY = now.first + dy[i];
+            int nextX = now.second + dx[i];
+
+            if(nextY<0||nextY>=maps.size()||nextX<0||nextX>=maps[0].size())
+                continue;
+            if(visited2[nextY][nextX]||maps[nextY][nextX]=='X')
+                continue;
+            q2.push({nextY, nextX});
+            visited2[nextY][nextX] = true;
+            count++;
+        }
+    }
+
+    if(!checkEnd)
         return -1;
-    return r1+r2;
+    else{
+        return count;
+    }
 }
 
