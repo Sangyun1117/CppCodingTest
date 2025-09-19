79eb2a49d10e8a4fa8722020c178b03d9ed3eac2
sangyun lee <115783758+Sangyun1117@users.noreply.github.com>
2025-09-19 09:05:15 +0900

    [level 1] Title: 추억 점수, Time: 0.77 ms, Memory: 4.46 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    unordered_map <string,int> map;
+    
+    for(int i=0; i<name.size(); ++i){
+        map.insert(make_pair(name[i], yearning[i]));
+    }
+    for(int i=0; i<photo.size(); ++i){
+        int sum = 0;
+        for(int j=0; j<photo[i].size(); ++j){
+            
+            if(map.find(photo[i][j])!=map.end()){
+                sum+=map.find(photo[i][j])->second;
+            }
+        }
+        answer.push_back(sum);
+    }
+    return answer;
+}
